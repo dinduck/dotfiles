d3bea1b131aad8a1697ccde53d15e2ef65e771d0
dingduck <kfove.liang@gmail.com>
2024-04-01 22:30:49 +0800

    first init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.luckynix = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = inputs;
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
