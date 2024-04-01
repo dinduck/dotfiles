52aef117891dbcfe03b172a74102506f52d1a66e
dingduck <kfove.liang@gmail.com>
2024-04-01 23:42:10 +0800

    add nixvim

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:dinduck/nixvim/6305363fd41e7c6460426d03075097fbc24631ad";
   };
 
   outputs = {
@@ -22,6 +23,14 @@
       };
       modules = [
         ./nixos/configuration.nix
+	        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.dingduck = import ./home/home.nix;
+          home-manager.extraSpecialArgs = inputs;
+        }
+
       ];
     };
   };
