a128b9a94f380609211e9a864aa744a394d61eb7
dingduck <kfove.liang@gmail.com>
2024-04-05 00:14:00 +0800

    add steam

--- nixos/modules/programs/steam.nix
+++ nixos/modules/programs/steam.nix
@@ -20,7 +20,7 @@ in {
       package = pkgs.steam;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-      steam-hardware.enable = true;
     };
+    hardware.steam-hardware.enable = true;
   };
 }
