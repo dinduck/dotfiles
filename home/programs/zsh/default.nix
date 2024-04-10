804fde57b64783b55557a51c7ce9cb869432f049
dingduck <kfove.liang@gmail.com>
2024-04-10 20:15:35 +0800

    add some plug for zsh

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -3,41 +3,44 @@
   lib,
   config,
   ...
-}: 
-let cfg = config.modules.programs.zsh;
-in
-{
-options.modules.programs.zsh = {
-enable = lib.mkOption {
-default = false;
-type = lib.types.bool;
-description = "my zsh config";
-};
-};
-config = lib.mkIf cfg.enable {
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      ll = "exa -l";
-      update = "sudo nixos-rebuild switch";
-    };
-    zplug = {
-      enable = true;
-      plugins = [
-        {name = "zsh-users/zsh-autosuggestions";}
-        {name = "zsh-users/zsh-syntax-highlighting";}
-        {
-          name = "zsh-users/zsh-history-substring-search";
-          tags = [as:plugin];
-        }
-        {
-          name = "MAHcodes/distro-prompt";
-          tags = [as:theme depth:1];
-        }
-        {name = "zap-zsh/supercharge";}
-        {name = "zap-zsh/exa";}
-      ];
+}: let
+  cfg = config.modules.programs.zsh;
+in {
+  options.modules.programs.zsh = {
+    enable = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+      description = "my zsh config";
     };
   };
+  config = lib.mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      shellAliases = {
+        ll = "exa -l";
+        update = "sudo nixos-rebuild switch --flake .";
+      };
+      zplug = {
+        enable = true;
+        plugins = [
+          {name = "zsh-users/zsh-autosuggestions";}
+          {name = "zsh-users/zsh-syntax-highlighting";}
+          {name = "zap-zsh/completions";}
+          {name = "Aloxaf/fzf-tab";}
+          {name = "Freed-Wu/fzf-tab-source";}
+          {name = "nix-community/nix-zsh-completions";}
+          {
+            name = "zsh-users/zsh-history-substring-search";
+            tags = [as:plugin];
+          }
+          {
+            name = "MAHcodes/distro-prompt";
+            tags = [as:theme depth:1];
+          }
+          {name = "zap-zsh/supercharge";}
+          {name = "zap-zsh/exa";}
+        ];
+      };
+    };
   };
 }
