33c909db4b1e49db689680e9986409c4bdd61d30
dingduck <kfove.liang@gmail.com>
2024-04-14 16:20:40 +0800

    cmsis-dap

---
+++ nixos/modules/services/udev.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.modules.services.udev;
+in {
+  options.modules.services.udev = {
+    enable = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+      description = "Enable obs";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.udev = {
+      enable = true;
+      extraRules = ''
+        # CMSIS DAP
+        ATTRS{idVendor}=="0d28", ATTRS{idProduct}=="0204", MODE="664", GROUP="plugdev", TAG+="uaccess"
+      '';
+    };
+  };
+}
